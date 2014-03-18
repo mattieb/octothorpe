af9c25727cafe03bb3963e73ca67c35b4abc4677
Matt Behrens <matt@zigg.com>
2014-03-18 18:33:22 -0400

    reference reST README from setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2014 Matt Behrens <matt@zigg.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 
 from distutils.core import setup
 
@@ -7,6 +22,7 @@ setup(
     name='octothorpe',
     version='0.4',
     description='Asterisk Manager Interface library for the Twisted engine',
+    long_description=open('README.rst', 'r').read(),
     author='Matt Behrens',
     author_email='matt@zigg.com',
     url='http://www.zigg.com/code/octothorpe/',
